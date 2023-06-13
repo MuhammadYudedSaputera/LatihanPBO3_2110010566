a672f7dd39cf4ca0b340e6cf00cf8694b294ee5e
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-13 11:46:34 +0800

    menjalankan program main lagi

--- src/geometri/Lingkaran.java
+++ src/geometri/Lingkaran.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package geometri;
 
-/**
- *
- * @author ASUS
- */
+
 public class Lingkaran {
-    
+  private double radius;
+
+    public Lingkaran(double radius) {
+        this.radius = radius;
+    }
+
+    public double luas() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double keliling() {
+        return 2 * Math.PI * radius;
+    }
 }
--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -3,13 +3,19 @@ package geometri;
 
 
 public class PersegiPanjang {
-    double panjang, lebar;
-    
-    public double luas (){
-        return 3.14*(panjang+lebar);
+   private double panjang;
+    private double lebar;
+
+    public PersegiPanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
     }
-    public double keliling (){
-        return 2*3.14*radius;
+
+    public double luas() {
+        return panjang * lebar;
     }
-    
+
+    public double keliling() {
+        return 2 * (panjang + lebar);
+    }
 }
