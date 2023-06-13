96ce482fb3dabd193756a21cc359e2d91796cf3a
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-13 10:26:48 +0800

    Membuat Class Lingkaran dan PersegiPanjang

--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package geometri;
 
-/**
- *
- * @author ASUS
- */
+
 public class PersegiPanjang {
+    double panjang, lebar;
+    
+    public double luas (){
+        return 3.14*(panjang+lebar);
+    }
+    public double keliling (){
+        return 2*3.14*radius;
+    }
     
 }
